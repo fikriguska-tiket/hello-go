f6c76dd29e364d38c7fabd741924981a6fc62006
Bintang Fikriguska <bintang.fikriguska@tiket.com>
2022-03-25 11:05:31 +0700

    add hello go

---
+++ go.mod
@@ -0,0 +1,9 @@
+module hello
+
+go 1.13
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.4.0
+	github.com/prometheus/client_golang v1.12.1
+)
---
+++ hello.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var httpRequestsTotal = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Name:        "http_requests_total",
+		Help:        "Total number of HTTP requests",
+		ConstLabels: prometheus.Labels{"server": "api"},
+	},
+)
+
+func helloServer(w http.ResponseWriter, r *http.Request) {
+	httpRequestsTotal.Inc()
+	hostname, err := os.Hostname()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Fprintf(w, fmt.Sprintf("Hello World! - from %s\n", hostname))
+	log.Println(fmt.Sprintf("Hello World! - from %s\n", hostname))
+}
+
+func main() {
+	prometheus.MustRegister(httpRequestsTotal)
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", helloServer)
+	r.Handle("/metrics", promhttp.Handler())
+
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+
+	addr := fmt.Sprintf(":%s", port)
+	srv := &http.Server{
+		Addr:    addr,
+		Handler: r,
+	}
+	log.Print("Starting server at ", addr)
+	log.Fatal(srv.ListenAndServe())
+}
